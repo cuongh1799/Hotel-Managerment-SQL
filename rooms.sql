ab59088f962b5e7bfd3db437de8f0f08606c082a
cuongh1799 <cuongghuynhh1009@gmail.com>
2023-08-26 13:58:28 +0700

    booking record and update room + services

--- rooms.sql
+++ rooms.sql
@@ -1,13 +1,13 @@
 -- NOTE: BOOLEAN DOESN'T EXIST IN ORACLE, SO SOLUTION IS varchar(1) WHICH IS EITHER 'Y' OR 'N'
 
-create table room(rID int, category varchar(50), price_per_day int, availability varchar(1));
-insert into room values(100, 'Family', 500000, 'Y' );
-insert into room values(101, 'Business', 300000, 'N');
-insert into room values(102, 'Standard', 200000, 'Y');
-insert into room values(103, 'Standard', 250000, 'Y');
-insert into room values(104, 'Family', 450000, 'N');
-insert into room values(105, 'Business', 290000, 'Y');
-insert into room values(106, 'Business', 350000, 'Y');
-insert into room values(107, 'Standard', 230000, 'N');
-insert into room values(108, 'Family', 600000, 'Y');
-insert into room values(109, 'Business', 280000, 'N');
+create table room(rID int, category varchar(50), price_per_day int, availability varchar(1), Room_IMG varchar(300));
+insert into room values(100, 'Family', 500000, 'Y', 'https://www.landmarklondon.co.uk/wp-content/uploads/2019/05/Executive-Family-1800x1200-1.jpg' );
+insert into room values(101, 'Business', 300000, 'N', 'https://res.cloudinary.com/traveltripperweb/image/upload/c_limit,f_auto,h_2500,q_auto,w_2500/v1643732012/lsedhhc48clt6jqatp7x.jpg');
+insert into room values(102, 'Standard', 200000, 'Y', 'https://amorgoshotel.com/wp-content/uploads/2014/12/Amorgos-Standard-Room1-e1464286427430.jpg');
+insert into room values(103, 'Standard', 250000, 'Y', 'https://en.hoteltheflag.jp/overview/rooms/img/stt.jpg');
+insert into room values(104, 'Family', 450000, 'N', 'https://www.kellys.ie/wp-content/uploads/2022/01/family-room-at-kellys.jpg');
+insert into room values(105, 'Business', 290000, 'Y', 'https://res.cloudinary.com/traveltripperweb/image/upload/c_limit,f_auto,h_2500,q_auto,w_2500/v1643732012/lsedhhc48clt6jqatp7x.jpg');
+insert into room values(106, 'Business', 350000, 'Y', 'https://res.cloudinary.com/traveltripperweb/image/upload/c_limit,f_auto,h_2500,q_auto,w_2500/v1643732012/lsedhhc48clt6jqatp7x.jpg');
+insert into room values(107, 'Standard', 230000, 'N', 'https://watermark.lovepik.com/photo/20211122/large/lovepik-deluxe-five-star-hotel-standard-room-picture_500759762.jpg');
+insert into room values(108, 'Family', 600000, 'Y', 'https://media.cntraveller.com/photos/629f1658f78438ff9ee3a25c/16:9/w_2580,c_limit/The%20Landmark-london-june22-pr-global.jpeg');
+insert into room values(109, 'Business', 280000, 'N', 'https://res.cloudinary.com/traveltripperweb/image/upload/c_limit,f_auto,h_2500,q_auto,w_2500/v1643732012/lsedhhc48clt6jqatp7x.jpg');
---
+++ services.sql
@@ -0,0 +1,14 @@
+create table services(
+	sID int,
+    Service_name varchar(100),
+    price int.
+    service_img varchar(3000
+);
+
+insert into services values(1, 'Breakfast buffet', 70000, 'https://images.pexels.com/photos/2612114/pexels-photo-2612114.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2');
+insert into services values(2, 'Rent car', 50000, 'https://images.pexels.com/photos/1213294/pexels-photo-1213294.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2');
+insert into services values(3, 'Rent motorbike', 40000, 'https://images.pexels.com/photos/5195654/pexels-photo-5195654.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2');
+insert into services values(4, 'Coffee', 20000, 'https://images.pexels.com/photos/7658124/pexels-photo-7658124.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2');
+insert into services values(5, 'Quick tour', 90000, 'https://images.pexels.com/photos/2407070/pexels-photo-2407070.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2');
+insert into services values(6, 'Rent BBQ equipment', 30000, 'https://images.pexels.com/photos/1191426/pexels-photo-1191426.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2');
+insert into services values(7, 'Coal', 20000, 'https://images.pexels.com/photos/2431651/pexels-photo-2431651.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2');
